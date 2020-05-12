a8c5554b1aca56915f3fdba88b364e950e7ff4ab
Anna Collins <annacollins@Annas-MBP.home>
2020-05-12 09:58:54 +0200

    chore: add part one

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import { ApolloServer, gql } from 'apollo-server'
+import db from "./db";
+
+const typeDefs = gql`
+  type Query {
+    users: [User!]!
+    posts: [Post!]!
+    user(id: ID!): User
+  }
+
+  type User {
+    name: String!
+    email: String!
+    age: Int
+    id: ID!
+  }
+
+  type Post {
+    title: String!
+    body: String!
+    id: ID!
+    published: Boolean!
+    author: User!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users() {
+      return db.users;
+    },
+
+    posts() {
+      return db.posts;
+    },
+    user(_, args) {
+      return db.users.find(user => user.id === args.id)
+    }
+  },
+
+  Post: {
+    author(post) {
+      return db.users.find(user => user.id === post.author);
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
