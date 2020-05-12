ec066b73f63a14e640b17a0e44cacf6a45c08a37
Anna Collins <annacollins@Annas-MBP.home>
2020-05-12 12:58:18 +0200

    chore: lint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { ApolloServer, gql } from 'apollo-server'
-import db from "./db";
+import db from "./db"
 
 const typeDefs = gql`
   type Query {
@@ -33,16 +33,15 @@ const typeDefs = gql`
     post: Post!
     author: User!
   }
-`;
+`
 
 const resolvers = {
   Query: {
     users() {
-      return db.users;
+      return db.users
     },
-
     posts() {
-      return db.posts;
+      return db.posts
     },
     user(_, args) {
       return db.users.find(user => user.id === args.id)
@@ -54,37 +53,37 @@ const resolvers = {
 
   User: {
     posts(user) {
-      return db.posts.filter(post => post.author === user.id);
+      return db.posts.filter(post => post.author === user.id)
     },
     comments(user) {
-      return db.comments.filter(comment => comment.author === user.id);
+      return db.comments.filter(comment => comment.author === user.id)
     }
   },
 
   Post: {
     author(post) {
-      return db.users.find(user => user.id === post.author);
+      return db.users.find(user => user.id === post.author)
     },
     comments(post) {
-      return db.comments.filter(comment => comment.post === post.id);
+      return db.comments.filter(comment => comment.post === post.id)
     }
   },
 
   Comment: {
     post(comment) {
-      return db.posts.find(post => post.id === comment.post);
+      return db.posts.find(post => post.id === comment.post)
     },
     author(comment) {
-      return db.users.find(user => user.id === comment.author);
+      return db.users.find(user => user.id === comment.author)
     }
   }
-};
+}
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
-});
+})
 
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+  console.log(`🚀  Server ready at ${url}`)
+})
