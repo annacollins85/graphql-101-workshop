c53c8233e72a896e170cc8c3c343e7fd64c1e76a
Anna Collins <annacollins@Annas-MBP.home>
2020-05-12 12:19:45 +0200

    chore: add answers to part 1

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const typeDefs = gql`
     users: [User!]!
     posts: [Post!]!
     user(id: ID!): User
+    post(id: ID!): Post
   }
 
   type User {
@@ -13,6 +14,8 @@ const typeDefs = gql`
     email: String!
     age: Int
     id: ID!
+    posts: [Post!]!
+    comments: [Comment!]!
   }
 
   type Post {
@@ -20,6 +23,14 @@ const typeDefs = gql`
     body: String!
     id: ID!
     published: Boolean!
+    author: User!
+    comments: [Comment!]!
+  }
+
+  type Comment {
+    id: ID!
+    text: String!
+    post: Post!
     author: User!
   }
 `;
@@ -35,12 +46,36 @@ const resolvers = {
     },
     user(_, args) {
       return db.users.find(user => user.id === args.id)
+    },
+    post(_, args) {
+      return db.posts.find(post => post.id === args.id)
+    }
+  },
+
+  User: {
+    posts(user) {
+      return db.posts.filter(post => post.author === user.id);
+    },
+    comments(user) {
+      return db.comments.filter(comment => comment.author === user.id);
     }
   },
 
   Post: {
     author(post) {
       return db.users.find(user => user.id === post.author);
+    },
+    comments(post) {
+      return db.comments.filter(comment => comment.post === post.id);
+    }
+  },
+
+  Comment: {
+    post(comment) {
+      return db.posts.find(post => post.id === comment.post);
+    },
+    author(comment) {
+      return db.users.find(user => user.id === comment.author);
     }
   }
 };
