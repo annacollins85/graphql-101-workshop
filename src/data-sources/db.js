488116e05bac1ac246222e5a5c07ed2dee07b25b
Anna Collins <annacollins@Annas-MacBook-Pro.local>
2020-07-08 16:06:12 +0200

    chore: add caching answers

--- src/data-sources/db.js
+++ src/data-sources/db.js
@@ -4,6 +4,14 @@ export class DB extends DataSource {
   constructor(db) {
     super();
     this.db = db;
+    this.cachedUsers = []
+    this.cachedUsersById = {}
+    this.cachedUsersByEmail = {}
+    this.cachedPostsByAuthor = {}
+    this.cachedCommentsByAuthor = {}
+    this.cachedPosts = []
+    this.cachedPostsById = {}
+    this.cachedCommentsByPost = {}
   }
 
   initialize({ context }) {
@@ -11,15 +19,30 @@ export class DB extends DataSource {
   }
 
   getUsers() {
-    return this.db.users
+    if (this.cachedUsers.length) {
+      return this.cachedUsers
+    }
+    const users = this.db.users
+    this.cachedUsers = users
+    return users
   }
 
   getUser(id) {
-    return this.db.users.find(user => user.id === id)
+    if (this.cachedUsersById[id]) {
+      return this.cachedUsersById[id]
+    }
+    const user = this.db.users.find(user => user.id === id)
+    this.cachedUsersById[id] = user
+    return user
   }
 
   getUserByEmail(email) {
-    return this.db.users.find(user => user.email === email)
+    if (this.cachedUsersByEmail[email]) {
+      return this.cachedUsersByEmail[email]
+    }
+    const user = this.db.users.find(user => user.email === email)
+    this.cachedUsersByEmail[email] = user
+    return user
   }
 
   createUser(user) {
@@ -27,19 +50,39 @@ export class DB extends DataSource {
   }
 
   getUserPosts(userId) {
-    return this.db.posts.filter(post => post.author === userId)
+    if (this.cachedPostsByAuthor[userId]) {
+      return this.cachedPostsByAuthor[userId]
+    }
+    const posts = this.db.posts.filter(post => post.author === userId)
+    this.cachedPostsByAuthor[userId] = posts
+    return posts
   }
 
   getUserComments(userId) {
-    return this.db.comments.filter(comment => comment.author === userId)
+    if (this.cachedCommentsByAuthor[userId]) {
+      return this.cachedCommentsByAuthor[userId]
+    }
+    const comments = this.db.comments.filter(comment => comment.author === userId)
+    this.cachedCommentsByAuthor[userId] = comments
+    return comments
   }
 
   getPosts() {
-    return this.db.posts
+    if (this.cachedPosts.length) {
+      return this.cachedPosts
+    }
+    const posts = this.db.posts
+    this.cachedPosts = posts
+    return posts
   }
 
-  getPost(id) {
-    return this.db.posts.find(post => post.id === id)
+  getPost(postId) {
+    if (this.cachedPostsById[postId]) {
+      return this.cachedPostsById[postId]
+    }
+    const post = this.db.posts.find(post => post.id === postId)
+    this.cachedPostsById[postId] = post
+    return post
   }
 
   createPost(post) {
@@ -47,7 +90,12 @@ export class DB extends DataSource {
   }
 
   getPostComments(postId) {
-    return this.db.comments.filter(comment => comment.post === postId)
+    if (this.cachedCommentsByPost[postId]) {
+      return this.cachedCommentsByPost[postId]
+    }
+    const comments = this.db.comments.filter(comment => comment.post === postId)
+    this.cachedCommentsByPost[postId] = comments
+    return comments
   }
 
   createComment(comment) {
